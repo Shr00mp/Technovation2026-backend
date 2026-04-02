342202e43638aacc20acda9e2a5953095f8d0ca2
Shr00mp <147839877+Shr00mp@users.noreply.github.com>
2026-04-02 21:26:23 +0100

    conversion function works

--- conversion.py
+++ conversion.py
@@ -1,3 +1,9 @@
-import pydub 
+from pydub import AudioSegment
+import os
 
+def convert(input_file, output_file):
+    audio = AudioSegment.from_file(input_file)
+    audio.export(output_file, format="wav")
+
+convert("uploaded_mp3s/audio_recording_1775083046845.mp3", "converted_wavs/new_audio.wav")
 
